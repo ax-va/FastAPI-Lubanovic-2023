93cf39e00c3f7551d895244d45329601cb139073
ax-va <vasiliev.informatik@t-online.de>
2026-06-24 11:12:45 +0200

    update

--- 01--fastapi-tour/01-09--http-responses--model-types--response_model/web/tag.py
+++ 01--fastapi-tour/01-09--http-responses--model-types--response_model/web/tag.py
@@ -8,15 +8,17 @@ from service.tag import create_tag, get_tag
 app = FastAPI()
 
 
+# Even though we returned a `Tag`, `response_model` will convert it to a `TagOut`
+
 @app.post('/', response_model=TagOut)
-def create_one(tag_in: TagIn) -> TagOut:
+def create_one(tag_in: TagIn) -> Tag:
     tag_obj: Tag = Tag(tag=tag_in.tag, created=datetime.now(UTC), secret="123")
     create_tag(tag_obj)
     return tag_obj
 
 
 @app.get('/{tag_str}', response_model=TagOut)
-def get_one(tag_str: str) -> TagOut:
+def get_one(tag_str: str) -> Tag:
     tag_obj: Tag = get_tag(tag_str)
 
     if tag_obj is None:
