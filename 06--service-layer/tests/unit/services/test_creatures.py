2e9f89d8ed43dcd844fcb76f7fc3e40800a82f94
ax-va <vasiliev.informatik@t-online.de>
2026-06-30 09:52:02 +0200

    update

--- 06--service-layer/tests/unit/services/test_creatures.py
+++ 06--service-layer/tests/unit/services/test_creatures.py
@@ -1,32 +1,57 @@
+import pytest
 from app.models.creature import Creature
 from app.repositories import fake_creatures
 from app.services import creatures as code
 
-sample = Creature(
+yeti = Creature(
     name="Yeti",
     country="CN",
     area="Himalayas",
     description="Hirsute Himalayan",
     aka="Abominable Snowman",
 )
+bigfoot = Creature(
+    name="Bigfoot",
+    description="Yeti's Cousin Eddie",
+    country="US",
+    area="*",
+    aka="Sasquatch"
+)
 
 
 class Test:
     def setup_method(self):
-        self.initial_repo = code.repository
+        self._original_repository = code.repository
         code.repository = fake_creatures
 
     def teardown_method(self):
-        code.repository = self.initial_repo
+        code.repository = self._original_repository
 
-    def test_create(self):
+    @pytest.mark.parametrize(
+        "sample",
+        [yeti, bigfoot]
+    )
+    def test_create(self, sample: Creature):
         result = code.create(sample)
         assert result == sample
 
-    def test_get_one_positive(self):
-        result = code.get_one(1)
-        assert result == sample
+    @pytest.mark.positive
+    @pytest.mark.parametrize(
+        "creature_id, expected",
+        [
+            (1, yeti),
+            (2, bigfoot),
+        ]
+    )
+    def test_get_one_positive(self, creature_id: int, expected: Creature):
+        result = code.get_one(creature_id)
+        assert result == expected
 
-    def test_get_one_negative(self):
-        result = code.get_one(1000)
+    @pytest.mark.negative
+    @pytest.mark.parametrize(
+        "creature_id",
+        [999, 1000]
+    )
+    def test_get_one_negative(self, creature_id):
+        result = code.get_one(creature_id)
         assert result is None
